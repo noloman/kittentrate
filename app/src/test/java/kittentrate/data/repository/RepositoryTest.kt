4cde679e11b683e9a8912446c49be8b13455b8b9
Manuel Lorenzo <manulorenzop@gmail.com>
2018-02-19 08:53:18 +0100

    feature/mvvm - repository tests green

--- app/src/test/java/kittentrate/data/repository/RepositoryTest.kt
+++ app/src/test/java/kittentrate/data/repository/RepositoryTest.kt
@@ -1,58 +1,68 @@
 package kittentrate.data.repository
 
 
+import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.whenever
-import kittentrate.GameApplication
+import io.reactivex.Flowable
+import io.reactivex.Observable
+import io.reactivex.observers.TestObserver
+import io.reactivex.subscribers.TestSubscriber
 import kittentrate.RobolectricTestHelper
-import kittentrate.api.NetworkDataSourceImpl
-import kittentrate.db.Database
+import kittentrate.api.ApiService
+import kittentrate.db.PlayerScoreDao
 import kittentrate.repository.Repository
+import kittentrate.repository.datasource.SharedPreferencesDataSource
 import kittentrate.ui.score.PlayerScore
-import org.junit.After
 import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.mockito.ArgumentMatchers.eq
-import org.mockito.Mock
 import org.mockito.Mockito.verify
-import org.robolectric.RobolectricTestRunner
 
 /**
- * Created by Manuel Lorenzo on 18/11/2017.
+ * Copyright 2018 Manuel Lorenzo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-@RunWith(RobolectricTestRunner::class)
+
 class RepositoryTest : RobolectricTestHelper() {
     private lateinit var repository: Repository
-    @Mock
-    private lateinit var gameLocalDataSource: GameLocalDataSource
-    @Mock
-    private lateinit var networkDataSourceImpl: NetworkDataSourceImpl
-    @Mock
-    private lateinit var sharedPreferencesDataSource: GameDataSource.SharedPreferencesDataSource
-    @Mock
-    private lateinit var database: Database
-    @Mock
-    private lateinit var application: GameApplication.Companion
+    private val flickrApiMock = mock<ApiService>()
+    private val playerScoreDaoMock = mock<PlayerScoreDao>()
+    private val sharedPreferencesDataSource = mock<SharedPreferencesDataSource>()
 
     @Before
     fun setUp() {
-        repository = Repository.getInstance(
-                gameLocalDataSource,
-                networkDataSourceImpl,
-                sharedPreferencesDataSource)
-        whenever(GameApplication.database).thenReturn(database)
+        repository = Repository(flickrApiMock, playerScoreDaoMock, sharedPreferencesDataSource)
     }
 
-    @After
-    fun tearDown() {
-        Repository.destroy()
+    @Test
+    fun `when getting top scores from repository it should retrieve Pepe and 100 points from the local data source`() {
+        val listPlayerScore: List<PlayerScore> = mutableListOf(PlayerScore("Pepe", 100))
+        whenever(playerScoreDaoMock.getTopScores()).thenReturn(Flowable.just(listPlayerScore))
+
+        TestSubscriber<List<PlayerScore>>().apply {
+
+            repository.getTopScores().subscribe(this)
+
+            assertNoErrors()
+            assertValue(listPlayerScore)
+        }
     }
 
     @Test
     fun `trying to get top scores from repository should query the local data source`() {
-        repository.topScores
+        repository.getTopScores()
 
-        verify(gameLocalDataSource).topScores
+        verify(playerScoreDaoMock).getTopScores()
     }
 
     @Test
@@ -60,21 +70,30 @@ class RepositoryTest : RobolectricTestHelper() {
         val playerScore = PlayerScore("Pepe", 1)
         repository.addTopScore(playerScore)
 
-        verify(gameLocalDataSource).addTopScore(playerScore)
+        verify(playerScoreDaoMock).addTopScore(playerScore)
     }
 
     @Test
     fun `setting photo tag should use the SharedPreferencesDataSource`() {
-        repository.preferencesPhotoTag = "bla"
+        whenever(sharedPreferencesDataSource.getPreferencesPhotoTag()).thenReturn(Observable.just("bla"))
 
-        verify(sharedPreferencesDataSource).preferencesPhotoTag = eq("bla")
+        repository.setPreferencesPhotoTag("bla")
+
+        TestObserver<String>().apply {
+            sharedPreferencesDataSource.getPreferencesPhotoTag().subscribe(this)
+            assertNoErrors()
+            assertValue("bla")
+        }
     }
 
     @Test
     fun `getting the preferences photo tag should use the SharedPreferencesDataSource`() {
-        whenever(sharedPreferencesDataSource.preferencesPhotoTag).thenReturn("bla")
-        repository.preferencesPhotoTag
+        whenever(sharedPreferencesDataSource.getPreferencesPhotoTag()).thenReturn(Observable.just("bla"))
 
-        verify(sharedPreferencesDataSource).preferencesPhotoTag
+        TestObserver<String>().apply {
+            repository.getPreferencesPhotoTag().subscribe(this)
+            awaitTerminalEvent()
+            assertNoErrors().assertValue("bla")
+        }
     }
 }
